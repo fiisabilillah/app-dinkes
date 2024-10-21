f833621a08838d8d46deefce899053eeb87c44cd
fiisabilillah <officialteam548@gmail.com>
2024-10-22 04:39:37 +0700

    super logout

--- app/Http/Controllers/SuperController.php
+++ app/Http/Controllers/SuperController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SuperController extends Controller
 {
@@ -10,4 +11,22 @@ class SuperController extends Controller
     {
         return view('super.index');
     }
+
+    public function SuperLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        $notification = array(
+            'message' => 'Logout Successfully',
+            'alert-type' => 'info'
+        );
+
+        return redirect('/login')->with($notification);
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SuperController;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -21,12 +22,23 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-///// Admin Group Middleware
+///// Super Group Middleware
 Route::middleware(['auth','role:super'])->group(function(){
 Route::get('/super/dashboard', [SuperController::class, 'SuperDashboard'])->name('super.dashboard');
+
+Route::get('/super/logout', [SuperController::class, 'SuperLogout'])->name('super.logout');
+Route::get('/super/profile', [SuperController::class, 'SuperProfile'])->name('super.profile');
+Route::post('/super/profile/store', [SuperController::class, 'SuperProfileStore'])->name('super.profile.store');
+
+Route::get('/super/change/password', [SuperController::class, 'SuperChangePassword'])->name('super.change.password');
+Route::post('/super/password/update', [SuperController::class, 'SuperPasswordUpdate'])->name('super.password.update');
+
 });
+
+
 
 ///// Admin Group Middleware
 Route::middleware(['auth','role:admin'])->group(function(){
 Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
 });
+
