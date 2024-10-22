e22e659e1ae2bca8a333b2b6ac2660a4e5da26c2
fiisabilillah <officialteam548@gmail.com>
2024-10-22 07:03:36 +0700

    change and profile siper

--- app/Http/Controllers/SuperController.php
+++ app/Http/Controllers/SuperController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class SuperController extends Controller
 {
@@ -32,4 +34,79 @@ class SuperController extends Controller
     {
         return view('super.super_login');
     }
+
+    public function SuperProfile(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('super.super_profile_view',compact('profileData'));
+    }// End Method
+
+
+    public function SuperProfileStore(Request $request){
+
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->username = $request->username;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if ($request->file('photo')) {
+           $file = $request->file('photo');
+           @unlink(public_path('upload/super_images/'.$data->photo));
+           $filename = date('YmdHi').$file->getClientOriginalName();
+           $file->move(public_path('upload/super_images'),$filename);
+           $data['photo'] = $filename;
+        }
+
+        $data->save();
+
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+
+    }// End Method
+
+    public function SuperChangePassword(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('super.super_change_password',compact('profileData'));
+
+    }// End Method
+
+
+    public function SuperPasswordUpdate(Request $request){
+
+        /// Validation
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+        ]);
+
+        if (!Hash::check($request->old_password, auth::user()->password)) {
+
+            $notification = array(
+                'message' => 'Old Password Does not Match!',
+                'alert-type' => 'error'
+            );
+            return back()->with($notification);
+        }
+
+        /// Update The new Password
+        User::whereId(auth::user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+
+        $notification = array(
+            'message' => 'Password Change Successfully',
+            'alert-type' => 'success'
+        );
+        return back()->with($notification);
+
+    }// End Method
 }
