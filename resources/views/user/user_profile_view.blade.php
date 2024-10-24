af21a4bd2ade01c0d5a2009865f1e38933c8709a
fiisabilillah <officialteam548@gmail.com>
2024-10-24 12:22:00 +0700

    perbaikan di profil user

--- resources/views/user/user_profile_view.blade.php
+++ resources/views/user/user_profile_view.blade.php
@@ -26,7 +26,25 @@
         /* Warna latar belakang */
     }
 </style>
+
 <div class="page-content">
+    <!--breadcrumb-->
+    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+        <div class="breadcrumb-title pe-3">Profile Saya</div>
+        <!-- <div class="ps-3">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mb-0 p-0">
+                    <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">User Profile</li>
+                </ol>
+            </nav>
+        </div> -->
+        <div class="ms-auto">
+
+        </div>
+    </div>
+    <!--end breadcrumb-->
     <div class="container">
         <div class="main-body">
             <div class="row">
