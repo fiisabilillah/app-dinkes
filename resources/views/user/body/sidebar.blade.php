5656a1b7f6b9e687193014be8e83faa4f148c1c6
fiisabilillah <officialteam548@gmail.com>
2024-10-25 06:38:31 +0700

    upload apk

--- resources/views/user/body/sidebar.blade.php
+++ resources/views/user/body/sidebar.blade.php
@@ -1,136 +1,61 @@
-   <div class="sidebar-wrapper" data-simplebar="true">
-       <div class="sidebar-header">
-           <div>
-               <img src="{{ asset('backend/assets/images/logo-jambi.png') }}" class="logo-icon" alt="logo icon">
-           </div>
-           <div>
-               <h4 class="logo-text" style="color: darkgreen;">SDMK</h4>
-           </div>
-           <div class="toggle-icon ms-auto" style="color: black;"><i class='bx bx-menu'></i>
-           </div>
-       </div>
-       <!--navigation-->
-       <ul class="metismenu" id="menu">
-           <li>
-               <a href="{{ route('user.dashboard') }}">
-                   <div class="parent-icon"><i class='bx bx-home-alt'></i>
-                   </div>
-                   <div class="menu-title">Dashboard</div>
-               </a>
-           </li>
-           <li class="menu-label">Data Diri</li>
-           <li>
-               <a href="{{ route('user.profile') }}">
-                   <div class="parent-icon"><i class="bx bx-user"></i>
-                   </div>
-                   <div class="menu-title">Profil Saya</div>
-               </a>
-           </li>
-           <li>
-               <a href="#">
-                   <div class="parent-icon"><i class="bx bx-right-top-arrow-circle"></i>
-                   </div>
-                   <div class="menu-title">Naik Pangkat</div>
-               </a>
-           </li>
-           <li>
-               <a href="#">
-                   <div class="parent-icon"><i class="bx bx-label"></i>
-                   </div>
-                   <div class="menu-title">Naik Jenjang</div>
-               </a>
-           </li>
-           <li>
-               <a href="#">
-                   <div class="parent-icon"><i class="bx bx-book-reader"></i>
-                   </div>
-                   <div class="menu-title">Ukom</div>
-               </a>
-           </li>
+<div class="sidebar-wrapper" data-simplebar="true">
+    <div class="sidebar-header">
+        <div>
+            <img src="{{ asset('backend/assets/images/logo-jambi.png') }}" class="logo-icon" alt="logo icon">
+        </div>
+        <div>
+            <h4 class="logo-text" style="color: darkgreen;">SDMK</h4>
+        </div>
+        <div class="toggle-icon ms-auto" style="color: black;"><i class='bx bx-menu'></i>
+        </div>
+    </div>
+    <!--navigation-->
+    <ul class="metismenu" id="menu">
+        <li>
+            <a href="{{ route('user.dashboard') }}">
+                <div class="parent-icon"><i class='bx bx-home-alt'></i>
+                </div>
+                <div class="menu-title">Dashboard</div>
+            </a>
+        </li>
+        <li class="menu-label">Data Diri</li>
+        <li>
+            <a href="{{ route('user.profile') }}">
+                <div class="parent-icon"><i class="bx bx-user"></i>
+                </div>
+                <div class="menu-title">Profil Saya</div>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <div class="parent-icon"><i class="bx bx-right-top-arrow-circle"></i>
+                </div>
+                <div class="menu-title">Naik Pangkat</div>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <div class="parent-icon"><i class="bx bx-label"></i>
+                </div>
+                <div class="menu-title">Naik Jenjang</div>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <div class="parent-icon"><i class="bx bx-book-reader"></i>
+                </div>
+                <div class="menu-title">Ukom</div>
+            </a>
+        </li>
 
-           <!--
-           <li class="menu-label">UI Elements</li>
+        <!-- Link download APK -->
+        <li>
+            <a href="{{ asset('upload/apk/sifunkes.apk') }}" download>
+                <div class="parent-icon"><i class="bx bx-download"></i>
+                </div>
+                <div class="menu-title">Download APK</div>
+            </a>
+        </li>
 
-           <li>
-               <a href="javascript:;" class="has-arrow">
-                   <div class="parent-icon"><i class='bx bx-cart'></i>
-                   </div>
-                   <div class="menu-title">eCommerce</div>
-               </a>
-               <ul>
-                   <li> <a href="ecommerce-products.html"><i class='bx bx-radio-circle'></i>Products</a>
-                   </li>
-                   <li> <a href="ecommerce-products-details.html"><i class='bx bx-radio-circle'></i>Product Details</a>
-                   </li>
-               </ul>
-           </li>
-           <li>
-               <a class="has-arrow" href="javascript:;">
-                   <div class="parent-icon"><i class='bx bx-bookmark-heart'></i>
-                   </div>
-                   <div class="menu-title">Components</div>
-               </a>
-               <ul>
-                   <li> <a href="component-alerts.html"><i class='bx bx-radio-circle'></i>Alerts</a>
-                   </li>
-                   <li> <a href="component-accordions.html"><i class='bx bx-radio-circle'></i>Accordions</a>
-                   </li>
-
-               </ul>
-           </li>
-           <li class="menu-label">Forms & Tables</li>
-           <li>
-               <a class="has-arrow" href="javascript:;">
-                   <div class="parent-icon"><i class='bx bx-message-square-edit'></i>
-                   </div>
-                   <div class="menu-title">Forms</div>
-               </a>
-               <ul>
-                   <li> <a href="form-elements.html"><i class='bx bx-radio-circle'></i>Form Elements</a>
-                   </li>
-                   <li> <a href="form-input-group.html"><i class='bx bx-radio-circle'></i>Input Groups</a>
-                   </li>
-                   <li> <a href="form-radios-and-checkboxes.html"><i class='bx bx-radio-circle'></i>Radios &
-                           Checkboxes</a>
-                   </li>
-                   <li> <a href="form-layouts.html"><i class='bx bx-radio-circle'></i>Forms Layouts</a>
-                   </li>
-                   <li> <a href="form-validations.html"><i class='bx bx-radio-circle'></i>Form Validation</a>
-                   </li>
-                   <li> <a href="form-wizard.html"><i class='bx bx-radio-circle'></i>Form Wizard</a>
-                   </li>
-                   <li> <a href="form-text-editor.html"><i class='bx bx-radio-circle'></i>Text Editor</a>
-                   </li>
-                   <li> <a href="form-file-upload.html"><i class='bx bx-radio-circle'></i>File Upload</a>
-                   </li>
-                   <li> <a href="form-date-time-pickes.html"><i class='bx bx-radio-circle'></i>Date Pickers</a>
-                   </li>
-                   <li> <a href="form-select2.html"><i class='bx bx-radio-circle'></i>Select2</a>
-                   </li>
-                   <li> <a href="form-repeater.html"><i class='bx bx-radio-circle'></i>Form Repeater</a>
-                   </li>
-               </ul>
-           </li>
-           <li>
-               <a class="has-arrow" href="javascript:;">
-                   <div class="parent-icon"><i class="bx bx-grid-alt"></i>
-                   </div>
-                   <div class="menu-title">Tables</div>
-               </a>
-               <ul>
-                   <li> <a href="table-basic-table.html"><i class='bx bx-radio-circle'></i>Basic Table</a>
-                   </li>
-                   <li> <a href="table-datatable.html"><i class='bx bx-radio-circle'></i>Data Table</a>
-                   </li>
-               </ul>
-           </li>
-           <li>
-               <a href="#">
-                   <div class="parent-icon"><i class="bx bx-support"></i>
-                   </div>
-                   <div class="menu-title">Support</div>
-               </a>
-           </li>
-       </ul> -->
-           <!--end navigation-->
-   </div>
+    </ul>
+</div>
