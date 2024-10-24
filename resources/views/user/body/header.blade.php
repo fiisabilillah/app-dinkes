e169e3236d7d02dc14001c72c7782334ad92bde4
fiisabilillah <officialteam548@gmail.com>
2024-10-24 10:16:19 +0700

    perbaikan nama header user

--- resources/views/user/body/header.blade.php
+++ resources/views/user/body/header.blade.php
@@ -352,7 +352,7 @@
                 <a class="d-flex align-items-center nav-link dropdown-toggle gap-3 dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <img src="{{ (!empty($profileData->photo)) ? url('upload/user_images/'.$profileData->photo) : url('upload/no_image.jpg')}}" class="user-img" alt="user avatar">
                     <div class="user-info">
-                        <p class="user-name mb-0">{{ $profileData->name }}</p>
+                        <p class="user-name mb-0">{{ $profileData->nama }}</p>
                         <p class="designattion mb-0">{{ $profileData->email }}</p>
                     </div>
                 </a>
